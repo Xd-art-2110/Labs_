6ace053d44f986fe1397ef9b54aa5c58b0b6885a
Xd-art-2110 <egorgolubev21@gmail.com>
2024-11-02 21:30:30 +0300

    Лабораторная номер 3

---
+++ Лабораторная номер 3/Задание 2.py
@@ -0,0 +1,19 @@
+# TODO Напишите функцию find_common_participants
+from os.path import split
+
+
+def find_common_participants(str1, str2, spltr = ','):
+    str1 = str1.split(spltr)
+    str2 = str2.split(spltr)
+    set_str1 = set(str1)
+    set_str2 = set(str2)
+    all_str = list(set_str1.intersection(set_str2))
+    all_str.sort()
+    return all_str
+
+participants_first_group = "Иванов|Петров|Сидоров"
+participants_second_group = "Петров|Сидоров|Смирнов"
+
+
+# TODO Провеьте работу функции с разделителем отличным от запятой
+print(find_common_participants(participants_first_group, participants_second_group, spltr='|'))
